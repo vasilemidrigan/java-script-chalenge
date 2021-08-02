ab90086c0894bd29b9c3baf703e7f580626a9b52
Vasile Midrigan <111rigan111@gmail.com>
2021-08-03 00:38:12 +0100

    a chalenge passed

--- chalenge.js
+++ chalenge.js
@@ -1,10 +1,9 @@
-//                                CHALENGES
+//                                JavaScript Chalenge
 
 // Coding challenge #1: Print numbers from 1 to 10
 for(let i = 1; i < 11; i++) {
   console.log(i);
 }
-
 console.log('Coding challenge #1: Print numbers from 1 to 10');
 console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
 console.log('xxxxxxx S  O  L  V  E  D xxxxxxxxx');
@@ -15,49 +14,65 @@ console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
 for (let i = 0; i < 50; i++) {
   console.log(2 * i + 1);
 }
-
 console.log('Coding challenge #2: Print the odd numbers less than 100');
 console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
 console.log('xxxxxxx S  O  L  V  E  D xxxxxxxxx');
 console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
 
+
 // Coding challenge #3: Print the multiplication table with 7
-for (let i = 1; i < 11; i++) {
-  if(i === 1) {
-    console.log('7 x 1 = ' + (7 * i));
-  } else if (i === 2) {
-    console.log('7 x 2 = ' + (7 * i));
-  } else if (i === 3) {
-    console.log('7 x 3 = ' + (7 * i));
-  } else if (i === 4) {
-    console.log('7 x 4 = ' + (7 * i));
-  } else if (i === 5) {
-    console.log('7 x 5 = ' + (7 * i));
-  } else if (i === 6) {
-    console.log('7 x 6 = ' + (7 * i));
-  } else if (i === 7) {
-    console.log('7 x 7 = ' + (7 * i));
-  } else if (i === 8) {
-    console.log('7 x 8 = ' + (7 * i));
-  } else if (i === 9) {
-    console.log('7 x 9 = ' + (7 * i));
-  } else {
-    console.log('7 x 10 = ' + i);
-  }
+for (i = 1; i <= 10; i++) {
+  let seven = 7;
+  let result = seven * i;
+  console.log(`${seven} x ${i} = ${result}`);
 }
 console.log('Coding challenge #3: Print the multiplication table with 7');
 console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
 console.log('xxxxxxx S  O  L  V  E  D xxxxxxxxx');
 console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+
+
 // Coding challenge #4: Print all the multiplication tables with numbers from 1 to 10
-
-
-
-
-
-
-
-
+  for(i = 1; i <= 10; i++) {
+    let result = 1 * i;
+    console.log(`1 x ${i} = ${result}`);
+  }
+  for(i = 1; i <= 10; i++) {
+    let result = 2 * i;
+    console.log(`2 x ${i} = ${result}`);
+  }
+  for(i = 1; i <= 10; i++) {
+    let result = 3 * i;
+    console.log(`3 x ${i} = ${result}`);
+  }
+  for(i = 1; i <= 10; i++) {
+    let result = 4 * i;
+    console.log(`4 x ${i} = ${result}`);
+  }
+  for(i = 1; i <= 10; i++) {
+    let result = 5 * i;
+    console.log(`5 x ${i} = ${result}`);
+  }
+  for(i = 1; i <= 10; i++) {
+    let result = 6 * i;
+    console.log(`6 x ${i} = ${result}`);
+  }
+  for(i = 1; i <= 10; i++) {
+    let result = 7 * i;
+    console.log(`7 x ${i} = ${result}`);
+  }
+  for(i = 1; i <= 10; i++) {
+    let result = 8 * i;
+    console.log(`8 x ${i} = ${result}`);
+  }
+  for(i = 1; i <= 10; i++) {
+    let result = 9 * i;
+    console.log(`9 x ${i} = ${result}`);
+  }
+  for(i = 1; i <= 10; i++) {
+    let result = 10 * i;
+    console.log(`10 x ${i} = ${result}`);
+  }
 console.log('Coding challenge #4: Print all the multiplication tables with numbers from 1 to 10');
 console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
 console.log('xxxxxxx S  O  L  V  E  D xxxxxxxxx');
