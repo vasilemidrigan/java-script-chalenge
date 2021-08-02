356341fcde0a003487a53501ed529464808098c7
Vasile Midrigan <111rigan111@gmail.com>
2021-08-03 00:09:08 +0100

    first chalenges

---
+++ chalenge.js
@@ -0,0 +1,64 @@
+//                                CHALENGES
+
+// Coding challenge #1: Print numbers from 1 to 10
+for(let i = 1; i < 11; i++) {
+  console.log(i);
+}
+
+console.log('Coding challenge #1: Print numbers from 1 to 10');
+console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+console.log('xxxxxxx S  O  L  V  E  D xxxxxxxxx');
+console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+
+
+// Coding challenge #2: Print the odd numbers less than 100
+for (let i = 0; i < 50; i++) {
+  console.log(2 * i + 1);
+}
+
+console.log('Coding challenge #2: Print the odd numbers less than 100');
+console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+console.log('xxxxxxx S  O  L  V  E  D xxxxxxxxx');
+console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+
+// Coding challenge #3: Print the multiplication table with 7
+for (let i = 1; i < 11; i++) {
+  if(i === 1) {
+    console.log('7 x 1 = ' + (7 * i));
+  } else if (i === 2) {
+    console.log('7 x 2 = ' + (7 * i));
+  } else if (i === 3) {
+    console.log('7 x 3 = ' + (7 * i));
+  } else if (i === 4) {
+    console.log('7 x 4 = ' + (7 * i));
+  } else if (i === 5) {
+    console.log('7 x 5 = ' + (7 * i));
+  } else if (i === 6) {
+    console.log('7 x 6 = ' + (7 * i));
+  } else if (i === 7) {
+    console.log('7 x 7 = ' + (7 * i));
+  } else if (i === 8) {
+    console.log('7 x 8 = ' + (7 * i));
+  } else if (i === 9) {
+    console.log('7 x 9 = ' + (7 * i));
+  } else {
+    console.log('7 x 10 = ' + i);
+  }
+}
+console.log('Coding challenge #3: Print the multiplication table with 7');
+console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+console.log('xxxxxxx S  O  L  V  E  D xxxxxxxxx');
+console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+// Coding challenge #4: Print all the multiplication tables with numbers from 1 to 10
+
+
+
+
+
+
+
+
+console.log('Coding challenge #4: Print all the multiplication tables with numbers from 1 to 10');
+console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+console.log('xxxxxxx S  O  L  V  E  D xxxxxxxxx');
+console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
